738c30de912b9c3a3f425e1e2db6e1a0d3792f6e
Prabhakar Poudel <20899846+Prabhakar-Poudel@users.noreply.github.com>
2022-04-14 18:49:12 +0200

    handle editor loading

--- front/src/components/app/editor/editorBody/EditorBody.tsx
+++ front/src/components/app/editor/editorBody/EditorBody.tsx
@@ -1,18 +1,30 @@
 import MonacoEditor from '@monaco-editor/react'
+import { Skeleton } from '@mui/material'
 import { MonacoEditorOptions } from 'src/types/editorConfig'
 
 interface EditorBodyProps {
   defaultEditorOptions: MonacoEditorOptions
 }
 
+const LoadingEditor = () => (
+  <Skeleton
+    animation="wave"
+    width="100%"
+    height="100%"
+    variant="rectangular"
+    sx={{ bgcolor: 'grey.900' }}
+  />
+)
+
 const EditorBody = ({ defaultEditorOptions }: EditorBodyProps) => {
   return (
     <div className="grow flex">
       <MonacoEditor
-        defaultLanguage="javascript"
+        defaultLanguage="php"
         defaultValue=""
         theme="vs-dark"
         options={defaultEditorOptions}
+        loading={<LoadingEditor />}
       />
     </div>
   )
