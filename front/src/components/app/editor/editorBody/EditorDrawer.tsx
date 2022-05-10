d449728a38cbb2a9c356c6805fde01bb41cfc4c4
Prabhakar Poudel <20899846+Prabhakar-Poudel@users.noreply.github.com>
2022-05-10 20:58:19 +0200

    Add xterm integration

--- front/src/components/app/editor/editorBody/EditorDrawer.tsx
+++ front/src/components/app/editor/editorBody/EditorDrawer.tsx
@@ -1,23 +1,36 @@
 import Drawer from '@mui/material/Drawer'
+import { useEffect, useRef } from 'react'
+import { Terminal } from 'xterm'
+import 'xterm/css/xterm.css'
 
 interface EditorDrawerProps {
   open: boolean
 }
 
 const EditorDrawer = ({ open }: EditorDrawerProps) => {
+  const terminalRef = useRef<Terminal | null>(null)
+
+  useEffect(() => {
+    const container = document.getElementById('terminal-container')
+    if (!container || terminalRef.current) return
+    const term = new Terminal({ theme: { background: '#1e1e1e' }, cursorBlink: true, fontSize: 16, disableStdin: false })
+    term.open(container)
+    term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ')
+    terminalRef.current = term
+  }, [])
+
+  useEffect(() => {
+    if(open && terminalRef.current) terminalRef.current.focus()
+    else if(terminalRef.current) terminalRef.current.blur()
+  }, [open])
+
   return (
     <Drawer
       variant="persistent"
       anchor="right"
       open={open}
-      sx={{
-        flexShrink: 0,
-        [`& .MuiDrawer-paper`]: { boxSizing: 'border-box' },
-      }}
     >
-      <div className="p-4 pr-8">
-        This is the drawer and it belongs here
-      </div>
+      <div id="terminal-container" className="h-full" />
     </Drawer>
   )
 }
--- front/src/components/app/editor/editorBody/EditorSticky.tsx
+++ front/src/components/app/editor/editorBody/EditorSticky.tsx
@@ -7,8 +7,8 @@ interface EditorStickyProps {
 
 const EditorSticky = ({ onClick }: EditorStickyProps) => {
   return (
-    <Paper square elevation={4} className="w-6 h-screen py-10 z-[1300] relative">
-      <Button variant="text" size="small" aria-label="open drawer" onClick={onClick} className="p-0 rotate-90 -translate-x-5" color="secondary" disableRipple disableElevation>Open</Button>
+    <Paper square elevation={4} className="w-8 h-screen p-0 pt-20 z-[1300] relative">
+      <Button variant="text" size="small" aria-label="open drawer" onClick={onClick} className="!p-0 rotate-90 -translate-x-5" color="secondary" disableRipple disableElevation>Terminal</Button>
     </Paper>
   )
 }
