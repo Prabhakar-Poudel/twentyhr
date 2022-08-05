346fc0ee6d52846978f6b24dda180a56f3abf252
Prabhakar Poudel <20899846+Prabhakar-Poudel@users.noreply.github.com>
2022-08-05 19:21:01 +0200

    fix port issue

--- front/src/config/application.ts
+++ front/src/config/application.ts
@@ -1,6 +1,6 @@
-const port = process.env.REACT_APP_API_PORT
-const url = port
-  ? `${process.env.REACT_APP_API_HOST}:${process.env.REACT_APP_API_PORT}`
-  : `${process.env.REACT_APP_API_HOST}`
+const url =
+  process.env.NODE_ENV !== 'production'
+    ? `${process.env.REACT_APP_API_HOST}:${process.env.REACT_APP_API_PORT}`
+    : `${process.env.REACT_APP_API_HOST}`
 export const rootUrl = `http://${url}`
 export const wsUrl = `ws://${url}/cable`
