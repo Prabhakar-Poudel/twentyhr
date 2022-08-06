6d7aa5747e082d436e4787825619126d2f7248a9
Prabhakar Poudel <20899846+Prabhakar-Poudel@users.noreply.github.com>
2022-08-06 14:52:26 +0200

    update server protocol

--- front/src/config/application.ts
+++ front/src/config/application.ts
@@ -2,5 +2,6 @@ const url =
   process.env.NODE_ENV !== 'production'
     ? `${process.env.REACT_APP_API_HOST}:${process.env.REACT_APP_API_PORT}`
     : `${process.env.REACT_APP_API_HOST}`
-export const rootUrl = `http://${url}`
+const protocol = process.env.NODE_ENV !== 'production' ? 'https' : 'http'
+export const rootUrl = `${protocol}://${url}`
 export const wsUrl = `ws://${url}/cable`
