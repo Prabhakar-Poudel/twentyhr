568a6ef071eba67bc1bbfc90aae97f912aa66009
Prabhakar Poudel <20899846+Prabhakar-Poudel@users.noreply.github.com>
2022-08-07 00:20:21 +0200

    fix urls

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -7,10 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['http://localhost:3101', 'https://twentyhr.com']
+    origins ['http://localhost:3101', 'https://www.twentyhr.com']
 
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 end
 
-Rails.application.config.action_cable.allowed_request_origins = ['http://localhost:3101', 'https://twentyhr.com']
+Rails.application.config.action_cable.allowed_request_origins = ['http://localhost:3101', 'https://www.twentyhr.com']
