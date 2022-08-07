ff6cd267a5de719f7d5d07d20ae30d5c5f77e740
Prabhakar Poudel <20899846+Prabhakar-Poudel@users.noreply.github.com>
2022-08-07 12:08:45 +0200

    update confirmation url

--- api/app/helpers/application_helper.rb
+++ api/app/helpers/application_helper.rb
@@ -3,6 +3,10 @@ module ApplicationHelper
     "#{web_client_url}reset-password?token=#{token}"
   end
 
+  def verify_account_link(token:)
+    "#{web_client_url}verify-account?token=#{token}"
+  end
+
   def web_client_url
     ENV.fetch("RAILS_ENV", "development") == "production" ? "https://www.twentyhr.com/" : "http://localhost:3101/"
   end
